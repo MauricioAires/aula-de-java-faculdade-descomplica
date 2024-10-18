fb806a5e5ffae50509a329ebb3356cc10b2b4b06
MauricioAires <mauricioaires015@gmail.com>
2024-10-18 12:42:40 -0300

    :cog: Informações de ambiente

---
+++ aula-04.java
@@ -0,0 +1,13 @@
+
+
+
+/**
+ *
+ *  - O java é compilado e gerado um arquivo .class, e esse arquivo é executado na JVM.
+ *  - JVM - Java Virtual Machine
+ *  - Mine ecossistema para a execução do programa
+ *  - JRE - Java Runtime Environment (é onde tem todos os recursos necessário para executar
+ * um programa java)
+ * - JDK - Java Development Kit (JRE e JVM já vem instalado dentro do JDK)
+ * - IDE - Integrated Development Environment (Ambiente de desenvolvimento integrado)
+ */
