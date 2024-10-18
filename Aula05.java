b043ee239112b8725534ae25f1c4bfe46481b5f8
MauricioAires <mauricioaires015@gmail.com>
2024-10-18 17:06:45 -0300

    :sparkles: Executando o projeto java manualmente

---
+++ Aula05.java
@@ -0,0 +1,17 @@
+public class Aula05 {
+
+  public static void main(String[] args) {
+
+    System.out.println("Hello, World!");
+
+  }
+}
+/**
+ *  Objetivo da aula, executar um projeto java manualmente
+ *
+ *  01. Criar o build do projeto
+ *  - javac NomeDoArquivo.java -> vai gerar um arquivo NomeDoArquivo.class
+ * 02. Executar o projeto
+ *  - java NomeDoArquivo.java
+ *
+ */
